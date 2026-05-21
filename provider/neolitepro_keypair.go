41b966341e50aa623a8de331b3d9a19aface2c90
Shirasaka Ren <ren@shirasaka.ren>
2026-05-21 13:17:00 +0700

    tambah neolitepro_keypair.go: struktur awal wkwk

--- provider/neolitepro_keypair.go
+++ provider/neolitepro_keypair.go
@@ -1,1 +1,35 @@
-// wip 0
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/pulumi/pulumi-go-provider/infer"
+
+	"github.com/biznetgio/pulumi-biznetgio/provider/client"
+)
+
+type NeoliteProKeypair struct{}
+
+type NeoliteProKeypairArgs struct {
+	Name string `pulumi:"name"`
+}
+
+type NeoliteProKeypairState struct {
+	NeoliteProKeypairArgs
+	KeypairID  int64  `pulumi:"keypairId"`
+	PublicKey  string `pulumi:"publicKey"`
+	PrivateKey string `pulumi:"privateKey" provider:"secret"`
+}
+
+func (a *NeoliteProKeypairArgs) Annotate(ann infer.Annotator) {
+	ann.Describe(&a.Name, "Nama keypair.")
+}
+
+func (s *NeoliteProKeypairState) Annotate(ann infer.Annotator) {
+	ann.Describe(&s.KeypairID, "Id keypair di BiznetGIO.")
+	ann.Describe(&s.PublicKey, "Public key yang di-generate.")
+	ann.Describe(&s.PrivateKey, "Private key (sensitive). Write-only: cuma ada di response create, ga bisa di-refetch.")
+}
+
