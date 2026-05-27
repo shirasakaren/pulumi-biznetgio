3e68e17dc4026191ef3256808e1eae27602b2b4b
Shirasaka Ren <ren@shirasaka.ren>
2026-05-27 13:36:00 +0700

    tambah objectstorage_functions.go: trigger attribute anjay

--- provider/objectstorage_functions.go
+++ provider/objectstorage_functions.go
@@ -1,1 +1,40 @@
-// wip 0
+package provider
+
+import (
+	"context"
+
+	"github.com/pulumi/pulumi-go-provider/infer"
+)
+
+type ObjectStorageInstances struct{}
+
+type ObjectStorageInstancesArgs struct {
+	Status *string `pulumi:"status,optional"`
+}
+
+type ObjectStorageInstancesResult struct {
+	Instances []ObjectStorageInstancesInstance `pulumi:"instances"`
+}
+
+type ObjectStorageInstancesInstance struct {
+	ID        string `pulumi:"id"`
+	Label     string `pulumi:"label"`
+	Status    string `pulumi:"status"`
+	ProductID int64  `pulumi:"productId"`
+	Quota     int64  `pulumi:"quota"`
+	Raw       string `pulumi:"raw" provider:"secret"`
+}
+
+func (a *ObjectStorageInstancesArgs) Annotate(ann infer.Annotator) {
+	ann.Describe(&a.Status, "Filter by status (Active, Pending, Suspended, Terminated).")
+}
+
+func (r *ObjectStorageInstancesResult) Annotate(ann infer.Annotator) {
+	ann.Describe(&r.Instances, "List of object storage instances.")
+}
+
+func (ObjectStorageInstances) Invoke(
+	ctx context.Context, req infer.FunctionRequest[ObjectStorageInstancesArgs],
+) (infer.FunctionResponse[ObjectStorageInstancesResult], error) {
+	c := GetClient(ctx)
+	items, err := c.ObjectStorage().AccountsList(ctx, osStr(req.Input.Status))
