3e90cbc577c169738885be781a71ea959c94b4bc
Shirasaka Ren <ren@shirasaka.ren>
2026-05-30 17:53:00 +0700

    tambah Program.cs: trigger attribute hehe

--- examples/dotnet/Program.cs
+++ examples/dotnet/Program.cs
@@ -1,21 +1,41 @@
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using Pulumi;
-using Boilerplate = Pulumi.ProviderBoilerplate;
+using Biznetgio.Biznetgio;
 
-return await Deployment.RunAsync(() => 
+return await Deployment.RunAsync(() =>
 {
-    var myRandomResource = new Boilerplate.Random("myRandomResource", new()
+    // token: `pulumi config set biznetgio:apiToken <token> --secret` atau env BIZNETGIO_API_KEY
+    var apiToken = Config.ApiToken;
+    if (string.IsNullOrEmpty(apiToken))
     {
-        Length = 24,
+        throw new Exception("set biznetgio:apiToken di config atau BIZNETGIO_API_KEY di env");
+    }
+
+    var products = NeoliteProducts.Invoke();
+    var osList = NeoliteOsList.Invoke(new NeoliteOsListInvokeArgs
+    {
+        ProductId = products.Apply(p => p.Products[0].ProductId),
     });
 
-    var myRandomComponent = new Boilerplate.RandomComponent("myRandomComponent", new()
+    var keypair = new NeoliteKeypair("demo-keypair", new NeoliteKeypairArgs
     {
-        Length = 24,
+        Name = "neo-lite-key",
     });
 
-    return new Dictionary<string, object?>{};
+    var vm = new NeoliteVm("demo-vm", new NeoliteVmArgs
+    {
+        VmName = "neo-lite-1",
+        ProductId = products.Apply(p => p.Products[0].ProductId),
+        SelectOs = osList.Apply(o => o.Oss[0].Name),
+        KeypairId = keypair.KeypairId,
+        Cycle = "m",
+        SshAndConsoleUser = "admin",
+        ConsolePassword = "s3cretP4ssw0rd",
+    });
+
+    return new Dictionary<string, object?>
+    {
+        ["vmStatus"] = vm.Status,
+    };
 });
-
-// wip 175
