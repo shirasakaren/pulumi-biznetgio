a03d4ea8d831380fec71be243fa2553c4ad28ad4
Shirasaka Ren <ren@shirasaka.ren>
2026-06-11 23:10:00 +0700

    struktur awal errors.go

--- provider/client/errors.go
+++ provider/client/errors.go
@@ -22,3 +22,27 @@ func (e *APIError) Error() string {
 }
 
 func IsNotFound(err error) bool {
+	var apiErr *APIError
+	return errors.As(err, &apiErr) && apiErr.StatusCode == http.StatusNotFound
+}
+
+func parseErrorBody(body []byte) (int, string) {
+	var m struct {
+		Code    int    `json:"code"`
+		Detail  any    `json:"detail"`
+		Message string `json:"message"`
+		Error   string `json:"error"`
+	}
+	if err := json.Unmarshal(body, &m); err != nil {
+		return 0, ""
+	}
+	msg := string(body)
+	if d, ok := m.Detail.(string); ok {
+		msg = d
+	} else if m.Message != "" {
+		msg = m.Message
+	} else if m.Error != "" {
+		msg = m.Error
+	}
+	return m.Code, msg
+}
