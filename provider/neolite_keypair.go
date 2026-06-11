68699e8ade2416180333c2e96c00c8faa6116a95
Shirasaka Ren <ren@shirasaka.ren>
2026-06-11 11:52:00 +0700

    crud dasar neolite_keypair.go cok

--- provider/neolite_keypair.go
+++ provider/neolite_keypair.go
@@ -35,3 +35,40 @@ func (s *NeoliteKeypairState) Annotate(ann infer.Annotator) {
 }
 
 func (NeoliteKeypair) WireDependencies(f infer.FieldSelector, _ *NeoliteKeypairArgs, state *NeoliteKeypairState) {
+	f.OutputField(&state.PrivateKey).AlwaysSecret()
+}
+
+func (NeoliteKeypair) Create(
+	ctx context.Context, req infer.CreateRequest[NeoliteKeypairArgs],
+) (infer.CreateResponse[NeoliteKeypairState], error) {
+	if req.DryRun {
+		return infer.CreateResponse[NeoliteKeypairState]{
+			ID:     "0",
+			Output: NeoliteKeypairState{NeoliteKeypairArgs: req.Inputs},
+		}, nil
+	}
+
+	c := GetClient(ctx)
+	// pake raw response: field private key undocumented, aliasnya bisa beda-beda.
+	raw, err := c.Neolite().KeypairCreateRaw(ctx, req.Inputs.Name)
+	if err != nil {
+		return infer.CreateResponse[NeoliteKeypairState]{}, err
+	}
+
+	keypairID := neoAliasInt(raw, "keypair_id", "neosshkey_id", "id")
+	if keypairID == 0 {
+		return infer.CreateResponse[NeoliteKeypairState]{},
+			fmt.Errorf("create neolite keypair response tidak ada keypair_id: %s", neoRawJSON(raw))
+	}
+	state := NeoliteKeypairState{NeoliteKeypairArgs: req.Inputs}
+	state.KeypairID = keypairID
+	state.PublicKey = neoAliasStr(raw, "public_key", "pubkey")
+	state.PrivateKey = neoAliasStr(raw, "private_key", "private", "secret_key", "pem")
+
+	return infer.CreateResponse[NeoliteKeypairState]{ID: strconv.FormatInt(keypairID, 10), Output: state}, nil
+}
+
+func (NeoliteKeypair) Read(
+	ctx context.Context, req infer.ReadRequest[NeoliteKeypairArgs, NeoliteKeypairState],
+) (infer.ReadResponse[NeoliteKeypairArgs, NeoliteKeypairState], error) {
+	c := GetClient(ctx)
